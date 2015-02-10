d0d0c016f1e0fb1126e9a58f3385f6e58c9debf6
boez <boez7878@gmail.com>
2015-02-10 23:32:57 +0300

    First commit

---
+++ php.loc/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset =utf-8"/>
+<title>Страница сайта</title>
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<?php
+$f= 32;
+$g=35;
+echo ($f * $g);
+
+?>
+</body>
+</html>
+
